c2ada6959cc9f3311e67dc56976f09223bb026f9
abirukov2008 <abirukov2008@gmail.com>
2022-12-27 13:35:15 +0300

    add blueprints

---
+++ webapp/news/views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, current_app, render_template
+from webapp.weather import weather_by_city
+from webapp.news.models import News
+
+blueprint = Blueprint('news', __name__)
+
+
+@blueprint.route("/")
+def index():
+    title = "Новости Python"
+    weather_param = weather_by_city(current_app.config["WEATHER_DEFAULT_CITY"])
+    news_list = News.query.order_by(News.published.desc()).all()
+    return render_template(
+        "index.html",
+        weather_param=weather_param,
+        page_title=title,
+        news_list=news_list,
+    )
